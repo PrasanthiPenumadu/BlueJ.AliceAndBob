054c5c2ed4699fcdd340d2dc7b013be24b6ebc70
Prasanthi Penmadu <prasanthipenmadu@zipcoders-MacBook-Pro-10.local>
2019-05-21 18:11:27 -0400

    Commiting Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Alice and Bob
  */
+import java.util.Scanner;
 public class Main {
-
+   
     public static void main(String[] args ){
-
+         String a;
+  
+        Scanner ui=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        a=ui.next();
+        
+        if(a.equals("Alice")||a.equals("alice"))
+        {
+             System.out.println("Hello Alice ");
+        }
+        
+         else if(a.equals("Bob")||a.equals("bob"))
+        {
+             System.out.println("Hello Bob ");
+        }
+        else
+          System.out.println("Hello there ");
+        
     }
 }
